ee471dc98e4ebf86c8566f158c6681460007a6ed
Dan Frenette <dan.r.frenette@gmail.com>
2026-02-21 10:38:19 -0500

    add test boilerplate, fixtures, and version spec
    - spec_helper with webmock, fixture helper
    
    - HTML fixtures for index and about pages

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "nous"
+require "webmock/rspec"
+
+WebMock.disable_net_connect!
+
+FIXTURE_PATH = File.expand_path("fixtures", __dir__)
+
+def fixture(name)
+  File.read(File.join(FIXTURE_PATH, name))
+end
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
